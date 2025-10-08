2ed5ec231960b453affbf3a528840803e248ab59
n-szilard <nagyapatiszilard@turr.hu>
2025-10-08 12:11:43 +0200

    Forgalom és kategória lista

--- src/app/components/categories/list/list.component.ts
+++ src/app/components/categories/list/list.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import axios, { AxiosError } from 'axios';
+import { CommonModule, NgForOf } from "@angular/common";
+
+interface Category {
+  id: number;
+  kategoriaNev: string;
+}
 
 @Component({
   selector: 'app-catlist',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgForOf, CommonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
-export class CatListComponent {
 
+
+export class CatListComponent implements OnInit{
+
+  categories: Category[] = [];
+
+  async ngOnInit() {
+    try {
+      const response = await axios.get('http://localhost:3000/categories');
+      this.categories = response.data;
+
+    } catch (error: any) {
+      console.log(error.message);
+      alert('Hiba történt az adatbázis elérésekor!')
+    }
+  }
 }
--- src/app/components/trafics/list/list.component.ts
+++ src/app/components/trafics/list/list.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import axios from 'axios';
+import { NgForOf } from "@angular/common";
+
+interface Traffic {
+  id: Number;
+  termek: string; 
+  vevo: string;
+  kategoriaId: number;
+  egyseg: string;
+  nettoar: number;
+  mennyiseg: number;
+  kiadva: boolean;
+  kategoriaNev: string;
+}
 
 @Component({
   selector: 'app-trafficlist',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgForOf],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
-export class TrafficListComponent {
+export class TrafficListComponent implements OnInit{
+  traffics: Traffic[] = [];
+  async ngOnInit() {
+    try {
+      const response = await axios.get('http://localhost:3000/traffic');
+      this.traffics = response.data;
 
+    } catch (error: any) {
+      console.log(error.message);
+      alert('Hiba történt az adatbázis elérésekor!')
+    }
+  }
 }
