13c260bccace21b0a98ef51e76d32bcac54fa02c
n-szilard <nagyapatiszilard@turr.hu>
2025-10-15 11:04:20 +0200

    Traffic form formázás

--- src/app/components/trafics/form/form.component.ts
+++ src/app/components/trafics/form/form.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Category } from '../../../interfaces/category';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { apiRES } from '../../../interfaces/apiRES';
+import { ApiService } from '../../../services/api.service';
+import { NgForOf } from "@angular/common";
+import { Traffic } from '../../../interfaces/traffic';
+import { FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-trafficForm',
   standalone: true,
-  imports: [],
+  imports: [NgForOf, RouterLink, FormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
 })
-export class TrafficFormComponent {
+export class TrafficFormComponent implements OnInit {
+  private router = inject(Router);
 
+  allCategories: Category[] = [];
+
+  newTraffic: Traffic = {
+    id: 0,
+    termek: '',
+    vevo: '',
+    kategoriaId: 0,
+    egyseg: '',
+    nettoar: 0,
+    mennyiseg: 0,
+    kiadva: false,
+    kategoriaNev: ''
+  }
+
+  constructor(
+    private api: ApiService,
+    private activatedRoute: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.getAllCategories();
+  }
+
+  getAllCategories() {
+    this.api.selectAll('categories').then((res: apiRES) => {
+      this.allCategories = res.data;
+    })
+  }
+
+  save() {
+    console.log('asd')
+  }
 }
